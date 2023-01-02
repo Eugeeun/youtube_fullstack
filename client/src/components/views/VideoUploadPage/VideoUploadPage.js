1d88ca51fab61954bc268c52e5665349b5c86225
JongWon <jack603@naver.com>
2023-01-03 05:51:29 +0900

    video and thumnail

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
+import Axios from 'axios';
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const videoPrivateOption = [
+  {
+    value: 0,
+    label: 'Private',
+  },
+  {
+    value: 1,
+    label: 'Public',
+  },
+];
+
+const videoCategoryOption = [
+  {
+    value: 0,
+    label: 'Film & Animation',
+  },
+  {
+    value: 1,
+    label: 'Autos & Vehicles',
+  },
+  {
+    value: 2,
+    label: 'Music',
+  },
+  {
+    value: 3,
+    label: 'Pets & Animals',
+  },
+];
+
 function VideoUploadPage() {
+  const [videoTitle, setVideoTitle] = useState('');
+  const [videoDesc, setVideoDesc] = useState('');
+  const [videoPrivate, setVideoPrivate] = useState(false);
+  const [videoCategory, setVideoCategory] = useState('Film & Animation');
+
+  const onTitleChange = event => setVideoTitle(event.target.value);
+  const onDescChange = event => setVideoDesc(event.target.value);
+  const onPrivateChange = event => setVideoPrivate(event.target.value);
+  const onCategoryChange = event => setVideoCategory(event.target.value);
+  const onDrop = files => {
+    let formData = new FormData();
+    const config = {
+      header: { 'content-type': 'multipart/form-data' },
+    };
+    formData.append('file', files[0]);
+    Axios.post('/api/video/uploadfiles', formData, config).then(response => {
+      if (response.data.success) {
+        let videoData = {
+          url: response.data.url,
+          fileName: response.data.fileName,
+        };
+        Axios.post('/api/video/thumbnail', videoData).then(response => {
+          if (response.data.success) {
+            console.log(response.data);
+          } else {
+            alert('썸네일 생성에 실패 했습니다.');
+          }
+        });
+      } else alert('비디오 업로드를 실패했습니다.');
+    });
+  };
+
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -14,7 +77,7 @@ function VideoUploadPage() {
       <Form onSubmit>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           {/* Drop zone */}
-          <Dropzone onDrop multiple maxSize>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
@@ -43,26 +106,34 @@ function VideoUploadPage() {
         <br />
 
         <label htmlFor=''>Title</label>
-        <Input onChange value />
+        <Input onChange={onTitleChange} value={videoTitle} />
 
         <br />
         <br />
 
         <label htmlFor=''>Description</label>
-        <TextArea onChange value />
+        <TextArea onChange={onDescChange} value={videoDesc} />
 
         <br />
         <br />
 
-        <select name='' id='' onChange>
-          <option key value=''></option>
+        <select name='' id='' onChange={onPrivateChange}>
+          {videoPrivateOption.map((option, index) => (
+            <option key={index} value={option.value}>
+              {option.label}
+            </option>
+          ))}
         </select>
 
         <br />
         <br />
 
-        <select name='' id='' onChange>
-          <option key value=''></option>
+        <select name='' id='' onChange={onCategoryChange}>
+          {videoCategoryOption.map((option, index) => (
+            <option key={index} value={option.value}>
+              {option.label}
+            </option>
+          ))}
         </select>
 
         <br />
---
+++ server/routes/video.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+// const { Video } = require('../models/User');
+
+const { auth } = require('../middleware/auth');
+const multer = require('multer');
+let ffmpeg = require('fluent-ffmpeg');
+
+/** Storage multer config */
+let storage = multer.diskStorage({
+  /** 파일을 저장할 위치 */
+  destination: (req, file, callback) => {
+    callback(null, 'uploads/');
+  },
+
+  filename: (req, file, callback) => {
+    callback(null, `${Date.now()}_${file.originalname}`);
+  },
+
+  fileFilter: (req, file, callback) => {
+    const ext = path.extname(file, originalname);
+    if (ext !== ' .mp4') {
+      return callback(req.status(400).end('only mp4 is allowed'), false);
+    }
+    callback(null, true);
+  },
+});
+
+const upload = multer({ storage: storage }).single('file');
+
+//=================================
+//             Video
+//=================================
+
+router.post('/uploadfiles', (req, res) => {
+  /** 비디오를 서버에 저장 */
+  upload(req, res, err => {
+    if (err) return res.json({ success: false, err });
+    return res.json({
+      success: true,
+      url: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+router.post('/thumbnail', (req, res) => {
+  /** 썸네일 생성을 하고 비디오 러닝타임을 가져오기 */
+
+  let filePath = '';
+  let fileDuration = '';
+  /** 비디오 정보 가져오기 */
+  ffmpeg.setFfmpegPath(
+    'C:\\Program Files\\ffmpeg\\ffmpeg-2022-12-29-git-d39b34123d-essentials_build\\bin\\ffmpeg.exe'
+  );
+  ffmpeg.ffprobe(req.body.url, function (err, metadata) {
+    console.dir(metadata);
+    console.log(metadata.format.duration);
+    fileDuration = metadata.format.duration;
+  });
+
+  /** 썸네일 생성 */
+  ffmpeg(req.body.url)
+    .on('filenames', function (filenames) {
+      console.log('will generate ' + filenames.join(','));
+      console.log(filenames);
+      filePath = 'uploads/thumbnails/' + filenames[0];
+    })
+    .on('end', function () {
+      return res.json({
+        success: true,
+        url: filePath,
+        fileDuration: fileDuration,
+      });
+    })
+    .on('error', function (err) {
+      console.error(err);
+      return res.json({ success: false, err });
+    })
+    .screenshots({
+      count: 3,
+      folder: 'uploads/thumbnails',
+      size: '320x240',
+      filename: 'thumbnail-%b.png',
+    });
+});
+
+module.exports = router;
