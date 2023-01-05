405752c213f7b8ffe976977df22eff8ba6bdcb24
JongWon <jack603@naver.com>
2023-01-06 00:26:52 +0900

    SideVideos

--- client/src/components/views/VideoDetailPage/Sections/SideVideo.js
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function SideVideo() {
-  return (
-    <div style={{ display: 'flex', marginBottom: '1rem', padding: '0 2rem' }}>
-      <div style={{ width: '40%', marginBottom: '1rem' }}>
-        <a href=''>
-          <img style={{ width: '100%' }} src='' alt='' />
-        </a>
+  const [sideVideos, setSideVideos] = useState([]);
+
+  useEffect(() => {
+    Axios.get('/api/video/getVideos').then(response => {
+      if (response.data.success) {
+        setSideVideos(response.data.videos);
+      } else {
+        alert('비디오 가져오기를 실패했습니다.');
+      }
+    });
+  }, []);
+
+  const renderSideVideo = sideVideos.map((video, index) => {
+    let minutes = Math.floor(video.duration / 60);
+    let seconds = Math.floor(video.duration - minutes * 60);
+
+    return (
+      <div
+        key={index}
+        style={{ display: 'flex', marginBottom: '1rem', padding: '0 2rem' }}
+      >
+        <div style={{ width: '40%', marginRight: '1rem' }}>
+          <a href=''>
+            <img
+              style={{ width: '100%', height: '100%' }}
+              src={`http://localhost:5000/${video.thumbnail}`}
+              alt='thumbnail'
+            />
+          </a>
+        </div>
+        <div style={{ width: '50%' }}>
+          <a style={{ color: 'gray' }} href=''>
+            <span style={{ fontSize: '1rem', color: 'black' }}>
+              {video.title}
+            </span>
+            <br />
+            <span>{video.writer.name}</span>
+            <br />
+            <span>{video.views} views</span>
+            <br />
+            <span>
+              {minutes} : {seconds}
+            </span>
+            <br />
+          </a>
+        </div>
       </div>
-    </div>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <div style={{ marginTop: '3rem' }} />
+      {renderSideVideo}
+    </React.Fragment>
   );
 }
 
